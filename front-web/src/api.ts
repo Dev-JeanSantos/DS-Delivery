d8549005decbeb5ecf75d991f976c4f538e1d761
Jean Santos <jeancbsan@gmail.com>
2021-01-08 20:40:23 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
-const API_URL = 'https://sds2-jeansantos.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken= process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
